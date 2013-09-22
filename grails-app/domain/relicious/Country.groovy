47a6902f658bcfb4e783fadab522f61decbe1c7a
Xavierwoo88 <lalaboi88@yahoo.com>
2013-09-22 19:03:51 +0800

    added codes for country Domain Class

--- grails-app/domain/relicious/Country.groovy
+++ grails-app/domain/relicious/Country.groovy
@@ -2,6 +2,17 @@ package relicious
 
 class Country {
 
+    String id
+    String country_name
+    
     static constraints = {
     }
+
+  static hasMany = [user:User]
+
+    String toString() {
+    return country_name 
+    }
+    
+
 }
